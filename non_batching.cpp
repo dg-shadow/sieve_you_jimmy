afd06adf3e1b775660587b9131bc370cc0ec1771
= <=>
2019-01-30 19:24:49 +0000

    works with batch of 1

--- non_batching.cpp
+++ non_batching.cpp
@@ -1,98 +1,115 @@
 #include <vector>
 #include <iostream>
+#include <cstdlib>
 
 using std::vector;
 using std::cout;
 using std::endl;
 
-bool run_sieve(int start, int run_size, vector<int> & primes, vector<bool> & sieve);
-void prepare_sieve(int run, int start, int run_size, vector<int> & primes, vector<bool> & sieve);
+typedef uint64_t myint_t;
+
+
+bool run_sieve(myint_t start, myint_t run_size, vector<myint_t> & primes, vector<bool> & sieve);
+void prepare_sieve(int run, myint_t start, myint_t run_size, vector<myint_t> & primes, vector<bool> & sieve);
 void print(vector<bool> & sieve);
-void print(vector<int> & primes);
+void print(vector<myint_t> & primes);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  int start = 0;
-  int run_size = 100000000;
-  int n_runs = 1;
+  myint_t start = 0;
+  myint_t run_size = atoi(argv[1]);
+  myint_t n_runs = (argc > 2 ? atoi(argv[2]) : 1);
   int run = 0;
-  vector<int> primes(1,2);
-  vector<bool> sieve(run_size);
+  vector<myint_t> primes(1,2);
+  
 
-  // for (int run = 0; run < n_runs; ++run)
-  // {
+  for (int run = 0; run < n_runs; ++run)
+  {
+
+    cout << "preparing for run " << run + 1 << endl;
+    vector<bool> sieve(run_size, true);
     prepare_sieve(run, start, run_size, primes, sieve);
+    cout << "looping\n";
     while (true)
     {
-      bool tested_to_end_of_sieve = run_sieve(start + run * n_runs, run_size, primes, sieve);
+      bool tested_to_end_of_sieve = run_sieve(start + run * run_size, run_size, primes, sieve);
       if (tested_to_end_of_sieve)
       {
         break;
       }
     }
-  // }
-  //print(primes);
-    cout << primes.size() << endl;
+    print(primes);
+    cout << primes.size() << " primes up to " << run_size * (run + 1) << " (non incluseive).\n\n";
+  }
+  
+    
   return 0;
 }
 
 void print(vector<bool> & sieve)
 {
-  for (int x = 0; x < sieve.size(); ++x)
+  for (myint_t x = 0; x < sieve.size(); ++x)
   {
     cout << x << ": " << sieve[x] << ", ";
   }
   cout << endl; 
 }
 
-void print(vector<int> & primes)
+void print(vector<myint_t> & primes)
 {
- for (int x = 0; x < primes.size(); ++x)
+ for (myint_t x = 0; x < primes.size(); ++x)
   {
     cout << primes[x] << ", ";
   }
   cout << endl;
 }
 
-void prune_sieve(int start, int run_size, int prime, vector<bool> & sieve)
+void prune_sieve(myint_t start, myint_t run_size, myint_t prime, vector<bool> & sieve)
 {
-  int first_index = start + (prime % run_size);
-  for (int x = first_index; x < run_size; x += prime)
+  myint_t first_index = start + (prime % run_size);
+  for (myint_t x = first_index; x < run_size; x += prime)
   {
     sieve[x] = false;
   }
 }
 
-void prepare_sieve(int run, int start, int run_size, vector<int> & primes, vector<bool> & sieve)
+void prepare_sieve(int run, myint_t start, myint_t run_size, vector<myint_t> & primes, vector<bool> & sieve)
 {
-  for (int x = 0; x < run_size; ++x)
-  {
-    sieve[x] = true;
-  }
   if (run == 0)
   {
     sieve[0] = false;
     sieve[1] = false;
   }
-  for (int prime_index = 0; prime_index < primes.size(); ++ prime_index)
+  else
   {
-    prune_sieve(start, run_size, primes[prime_index], sieve);
+    for (myint_t prime_index = 0; prime_index < primes.size(); ++ prime_index)
+    {
+      prune_sieve(start, run_size, primes[prime_index], sieve);
+    }
   }
-  // print(sieve);
+  print(sieve);
 }
 
-bool run_sieve(int start, int run_size, vector<int> & primes, vector<bool> & sieve)
+bool run_sieve(myint_t start, myint_t run_size, vector<myint_t> & primes, vector<bool> & sieve)
 {
-  int latest_prime = primes.back();
+  myint_t latest_prime = primes.back();
 
   // cout << "latest " << latest_prime << endl;
 
   prune_sieve(start, run_size, latest_prime, sieve);
 
-  int prime_test_index;
 
-  for (prime_test_index = latest_prime - start; prime_test_index < run_size; ++prime_test_index)
+  myint_t prime_test_index;
+
+  // cout << "start:     " << start << endl;
+  // cout << "latest:    " << latest_prime << endl;
+  // cout << "first pti: " << latest_prime % run_size  << endl;
+  // cout << "run size:  " << run_size << endl;
+
+
+  for (prime_test_index = latest_prime ; prime_test_index < run_size; ++prime_test_index)
   {
+    cout << "pti " << prime_test_index << endl;
     if (sieve[prime_test_index])
     {
       primes.push_back(start + prime_test_index);
